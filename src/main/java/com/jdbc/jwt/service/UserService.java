d368b7ae4d603e2badf0b6bb90f306c0c7cd73c5
Bekzat-Y <bekayrysbaev551@gmail.com>
2024-03-29 19:09:42 +0600

    first commit

---
+++ src/main/java/com/jdbc/jwt/service/UserService.java
@@ -0,0 +1,18 @@
+package com.jdbc.jwt.service;
+
+import com.jdbc.jwt.entity.Role;
+import com.jdbc.jwt.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    User saveUser(User user);
+
+    Role saveRole(Role role);
+
+    void addRoleToUser(String username, String roleName);
+
+    User getUser(String username);
+
+    List<User> getUsers();
+}
